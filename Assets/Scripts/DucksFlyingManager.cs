8f5af8bfa2622c231a8ccb18de8c1937d2dbae70
kliment <kliment.sokolov@uni-konstanz.de>
2017-11-12 18:27:04 +0100

    Spawning ducks randomly + changing direction

---
+++ Assets/Scripts/DucksFlyingManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DucksFlyingManager : MonoBehaviour {
+    private float x, y;
+    Vector3 forwardV;
+    // Use this for initialization
+    void Start()
+    {
+        UpdateRotation();
+        InvokeRepeating("UpdateRotation", 2f, 2f);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        transform.position += transform.forward*Time.deltaTime;
+	}
+
+    void UpdateRotation()
+    {
+        x = Random.Range(-60f, -20f);
+        y = Random.Range(0f, 360f);
+        transform.rotation = Quaternion.Euler(x, y, 0f);
+    }
+}
