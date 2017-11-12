7c514a3b295450eed5b1d7c38507004331f3346e
kliment <kliment.sokolov@uni-konstanz.de>
2017-11-12 17:43:16 +0100

    whatever

---
+++ Assets/Scripts/ShootScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootScript : MonoBehaviour {
+    public GameObject fireAnim;
+    private ParticleSystem partSys;
+	// Use this for initialization
+	void Start () {
+        partSys = fireAnim.GetComponent<ParticleSystem>();
+	}
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("shoot animation");
+            partSys.Play();
+        }
+        else
+        {
+            partSys.Stop();
+        }
+        
+	}
+}
