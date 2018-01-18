a5dc04764314b7fa44a4bce2a1d862e3b68e4dac
kliments <kliments@github.com>
2018-01-18 17:36:32 +0100

    switch of scenes

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,6 +7,7 @@ public class LevelManager : MonoBehaviour {
 
     public bool switchScenes;
     private Scene scene;
+    public GameObject duckManager;
     // Use this for initialization
     void Start () {
         scene = SceneManager.GetActiveScene();
@@ -15,9 +16,8 @@ public class LevelManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(scene.name == "MainScene" && switchScenes)
+		if(scene.name == "MainScene" && duckManager.GetComponent<DuckManager>().noOfDucksEscaped > 2)
         {
-            switchScenes = false;
             SceneManager.LoadScene("MainScene2", LoadSceneMode.Single);
         }
 	}
